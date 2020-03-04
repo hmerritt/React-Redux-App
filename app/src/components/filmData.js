7bdc780776057e7cc4a962a8c0d58041a46aea0a
Harry Merritt <harry@merritt.es>
2020-03-04 04:16:46 +0000

    Added year, rating and cast

--- app/src/components/filmData.js
+++ app/src/components/filmData.js
@@ -1,23 +1,42 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
-import { Container, Paper, Typography } from "@material-ui/core";
+import {
+    Container,
+    Paper,
+    Typography,
+    List,
+    ListItem,
+    ListItemAvatar,
+    ListItemText,
+    Avatar
+} from "@material-ui/core";
+import { Person } from "@material-ui/icons";
+import { Rating } from "@material-ui/lab";
 import { makeStyles } from "@material-ui/core/styles";
 import { getFilm } from "../actions";
 
-const FilmData = (props) => {
+const FilmData = props => {
 
     //  Get film object from redux store
     const film = props.film;
 
+    //  Fetch film data
     useEffect(() => {
-        //  Fetch film data
-        props.getFilm()
+        props.getFilm();
     }, []);
 
+    //  Style components
     const styles = makeStyles(theme => ({
         paper: {
             display: "block",
             padding: "40px"
+        },
+        ratingStars: {
+            verticalAlign: "bottom",
+            marginRight: "10px"
+        },
+        cast: {
+            margin: "20px 0"
         }
     }))();
 
@@ -25,11 +44,44 @@ const FilmData = (props) => {
         <div className="film-card">
             <Container>
                 <Paper className={styles.paper} elevation={2}>
+
+                    {/* Title (year) */}
                     <Typography variant="h2">
-                        {
-                            !film.isFetching ? film.title : "Loading film..."
-                        }
+                        {film.title} <small>({film.year})</small>
                     </Typography>
+
+                    {/* Rating */}
+                    <Typography variant="caption">
+                        <Rating
+                            className={styles.ratingStars}
+                            name="rating"
+                            value={film.rating}
+                            precision={0.5}
+                            max={10}
+                            readOnly
+                        />
+                        {film.rating}
+                    </Typography>
+
+                    {/* Cast */}
+                    <List className={styles.cast} dense>
+                        {film.cast.map((person, key) => {
+                            return (
+                                <ListItem>
+                                    <ListItemAvatar>
+                                        <Avatar>
+                                            <Person />
+                                        </Avatar>
+                                    </ListItemAvatar>
+                                    <ListItemText
+                                        primary={person.actor}
+                                        secondary={person.character}
+                                    />
+                                </ListItem>
+                            );
+                        })}
+                    </List>
+
                 </Paper>
             </Container>
         </div>
@@ -38,7 +90,7 @@ const FilmData = (props) => {
 
 export default connect(
     state => ({
-      film: state.film.data
-  }),
-  { getFilm }
+        film: state.film.data
+    }),
+    { getFilm }
 )(FilmData);
