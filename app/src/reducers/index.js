47cb9bed6c5ebbe7bbc330e66f4fc2d76330cc80
Harry Merritt <harry@merritt.es>
2020-03-04 03:12:49 +0000

    Created async reducer using start, success and failure

---
+++ app/src/reducers/index.js
@@ -0,0 +1,52 @@
+import {
+    FETCH_FILM_START,
+    FETCH_FILM_SUCCESS,
+    FETCH_FILM_FAILURE
+} from "../actions/";
+
+export const initialState = {
+    search: "",
+    film: {
+        error: "",
+        isFetching: false,
+        data: {}
+    }
+};
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FETCH_FILM_START:
+            return {
+                ...state,
+                film: {
+                    ...state.film,
+                    isFetching: true
+                }
+            }
+
+        case FETCH_FILM_SUCCESS:
+            return {
+                ...state,
+                film: {
+                    ...state.film,
+                    isFetching: false,
+                    data: {
+                        ...action.payload
+                    }
+                }
+            }
+
+        case FETCH_FILM_FAILURE:
+            return {
+                ...state,
+                film: {
+                    ...state.film,
+                    error: action.payload,
+                    isFetching: false
+                }
+            }
+
+        default:
+            return state;
+    }
+};
